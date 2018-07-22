91da6e56e9412e2efbb479c004e60b79588effba
tetete <tetetelego2013@gmail.com>
2018-07-22 11:10:30 +0900

    touch button

--- public/game/input.js
+++ public/game/input.js
@@ -1,6 +1,7 @@
 var keysPress=new Array(1000);
 var handle=0;
 var tilt=0;
+/*
 window.addEventListener('devicemotion', function (event) {
     var gv = event.accelerationIncludingGravity;
     tilt=gv.x/10;
@@ -12,10 +13,46 @@ window.addEventListener('devicemotion', function (event) {
     if(tilt>1)tilt=1;
     if(tilt<-1)tilt=-1;
     tilt=Math.asin(tilt);
-});
+});*/
 window.onkeydown = function (ev) {
     keysPress[ev.keyCode] = true;
 }
 window.onkeyup = function (ev) {
     keysPress[ev.keyCode] = false;
 }
+window.addEventListener('touchstart', function (e) {
+    if(e.touches){
+        tilt=0;
+        if(e.touches[0].pageX<window.innerWidth*0.3){
+            tilt=-1;
+        }
+        else if(e.touches[0].pageX>window.innerWidth*0.7){
+            tilt=1;
+        }
+    }
+    e.preventDefault();
+}, false);
+window.addEventListener('touchmove', function (e) {
+    if(e.touches){
+        tilt=0;
+        if(e.touches[0].pageX<window.innerWidth*0.3){
+            tilt=-1;
+        }
+        else if(e.touches[0].pageX>window.innerWidth*0.7){
+            tilt=1;
+        }
+    }
+    e.preventDefault();
+} false);
+window.addEventListener('touchcancel', function (e) {
+    if(e.touches){
+        tilt=0;
+    }
+    e.preventDefault();
+}, false);
+window.addEventListener('touchend', function (e) {
+    if(e.touches){
+        tilt=0;
+    }
+    e.preventDefault();
+}, false);
